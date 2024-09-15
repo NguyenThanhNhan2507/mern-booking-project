f2ff9150c1985a5a9c3111d3206958205e29d959
Nguyen Thanh Nhan <nhandaica125@gmail.com>
2024-09-15 15:06:53 +0700

    first commit

---
+++ backend/src/index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors'
+import 'dotenv/config'
+
+
+const app = express();
+const POST = 3000
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cors());
+
+
+
+
+app.listen(POST, ()=>{
+    console.log(`server running on localhost: ${POST}`)
+})
